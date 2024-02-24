e5f6ab68a68f6dd9f8de9c7a4ed6e86d9c95a20a
Luciano Borba <luhborbafilho@gmail.com>
2024-02-24 17:04:54 -0300

    feat: adicionando csv usando duckdb

---
+++ duckdb/teste01-duck-csv.py
@@ -0,0 +1,54 @@
+import duckdb
+import time
+import os
+
+# Registrando o tempo de início
+inicio = time.time()
+
+# Conectando ao banco de dados DuckDB
+con = duckdb.connect()
+
+# Carregando dados
+con.execute("CREATE TABLE dados AS SELECT * FROM read_csv('dados.csv');")
+
+# Criando nova coluna de Valor Total
+con.execute("ALTER TABLE dados ADD COLUMN ValorTotal DECIMAL(10,2)")
+con.execute("UPDATE dados SET ValorTotal = ValorProduto * Quantidade")
+
+# Verificando o total de vendas por produtos
+#total_vendas_produtos = con.execute("""
+#SELECT Produto, SUM(ValorTotal) AS TotalVendas
+#FROM dados
+#GROUP BY Produto
+#ORDER BY TotalVendas DESC
+#""").fetchall()
+
+# Verificando o total de vendas por clientes
+#total_vendas_cliente = con.execute("""
+##SELECT Cliente, SUM(ValorTotal) AS TotalVendas
+#FROM dados
+#GROUP BY Cliente
+#ORDER BY TotalVendas DESC
+#""").fetchall()
+
+# Salvando em CSV
+
+# Salvando dados-duck.csv
+con.execute("COPY (SELECT * FROM dados) TO 'data/csv/dados-duck.csv'")
+
+# Salvando total_vendas_produtos-duck.csv
+con.execute("COPY (SELECT Produto, SUM(ValorTotal) AS TotalVendas FROM dados GROUP BY Produto ORDER BY TotalVendas DESC) TO 'data/csv/dados-duck.csv'")
+
+# Salvando total_vendas_cliente-duck.csv
+con.execute("COPY (SELECT Cliente, SUM(ValorTotal) AS TotalVendas FROM dados GROUP BY Cliente ORDER BY TotalVendas DESC) TO 'data/csv/dados-duck.csv'")
+
+# Fechando a conexão com o banco de dados
+con.close()
+
+# Registrando o tempo de término
+fim = time.time()
+
+# Calculando o tempo de execução
+tempo_execucao = fim - inicio
+
+print(f"Tempo de execução: {tempo_execucao:.2f} segundos")
